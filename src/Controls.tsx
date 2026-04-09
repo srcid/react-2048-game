118ef17ac788398f4be84415e0e97d6eb3ba7869
Alcides Ribeiro <alcidesribeirosampaioneto@gmail.com>
2026-04-09 12:11:53 -0300

    feat(controls): added undo button

--- src/Controls.tsx
+++ src/Controls.tsx
@@ -1,46 +1,64 @@
-import { AiOutlineArrowDown, AiOutlineArrowLeft, AiOutlineArrowRight, AiOutlineArrowUp } from "react-icons/ai";
+import {
+  AiOutlineArrowDown,
+  AiOutlineArrowLeft,
+  AiOutlineArrowRight,
+  AiOutlineArrowUp,
+} from "react-icons/ai";
 import { useBoardStore } from "./store/useBoardStore";
 import { MdOutlinePlusOne } from "react-icons/md";
+import { FaUndoAlt } from "react-icons/fa";
+import clsx from "clsx";
 
 export default function Controls() {
-  const boardState = useBoardStore(state => state);
+  const boardState = useBoardStore((state) => state);
 
   return (
     <div className="mt-2 flex flex-row gap-2">
-        <div className="grid grid-rows-2 grid-cols-3 gap-1 h-20 aspect-video">
-          <div aria-hidden="true"></div>
-          <button
-            className="bg-gray-700 rounded-md p-1 flex items-center justify-center"
-            onClick={() => boardState.moveUp()}
-          >
-            <AiOutlineArrowUp className="text-zinc-100"/>
-          </button>
-          <div aria-hidden="true"></div>
-          <button
-            className="bg-gray-700 rounded-md p-1 flex items-center justify-center"
-            onClick={() => boardState.moveLeft()}
-          >
-            <AiOutlineArrowLeft className="fill-zinc-100"/>
-          </button>
-          <button
-            className="bg-gray-700 rounded-md p-1 flex items-center justify-center"
-            onClick={() => boardState.moveDown()}
-          >
-            <AiOutlineArrowDown className="fill-zinc-100"/>
-          </button>
-          <button
-            className="bg-gray-700 rounded-md p-1 flex items-center justify-center"
-            onClick={() => boardState.moveRight()}
-          >
-            <AiOutlineArrowRight className="fill-zinc-100"/>
-          </button>
-        </div>
+      <div className="grid grid-rows-2 grid-cols-3 gap-1 h-20 aspect-video">
+        <div aria-hidden="true"></div>
         <button
-          className="bg-gray-700 rounded-md p-1 flex items-center justify-center aspect-video"
-          onClick={() => boardState.addNew()}
+          className="bg-gray-700 rounded-md p-1 flex items-center justify-center"
+          onClick={() => boardState.moveUp()}
         >
-          <MdOutlinePlusOne className="fill-zinc-100" />
+          <AiOutlineArrowUp className="text-zinc-100" />
+        </button>
+        <div aria-hidden="true"></div>
+        <button
+          className="bg-gray-700 rounded-md p-1 flex items-center justify-center"
+          onClick={() => boardState.moveLeft()}
+        >
+          <AiOutlineArrowLeft className="fill-zinc-100" />
+        </button>
+        <button
+          className="bg-gray-700 rounded-md p-1 flex items-center justify-center"
+          onClick={() => boardState.moveDown()}
+        >
+          <AiOutlineArrowDown className="fill-zinc-100" />
+        </button>
+        <button
+          className="bg-gray-700 rounded-md p-1 flex items-center justify-center"
+          onClick={() => boardState.moveRight()}
+        >
+          <AiOutlineArrowRight className="fill-zinc-100" />
         </button>
       </div>
+      <button
+        className="bg-gray-700 rounded-md p-1 flex items-center justify-center aspect-video"
+        onClick={() => boardState.addNew()}
+      >
+        <MdOutlinePlusOne className="fill-zinc-100" />
+      </button>
+      <button
+        className="bg-gray-700 rounded-md p-1 flex items-center justify-center size-20"
+        onClick={() => boardState.undo()}
+      >
+        <FaUndoAlt
+          className={clsx({
+            "fill-zinc-100": boardState.boards.length > 1,
+            "fill-zinc-500": boardState.boards.length === 1,
+          })}
+        />
+      </button>
+    </div>
   );
 }

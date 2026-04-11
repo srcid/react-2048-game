800a96b0f3f5d99635256de6aef2f56376d5e35f
Alcides Ribeiro <alcidesribeirosampaioneto@gmail.com>
2026-04-11 09:08:13 -0300

    test(extensions): group tranpose extension tests and make use of function it instead test

--- test/utils/extensions.test.ts
+++ test/utils/extensions.test.ts
@@ -1,91 +1,93 @@
 import "../../src/utils/extensions.ts";
 
-test("transposes a square matrix", () => {
-  const input = [
-    [1, 2],
-    [3, 4],
-  ];
+describe("transpose extension", () => {
+  it("transposes a square matrix", () => {
+    const input = [
+      [1, 2],
+      [3, 4],
+    ];
 
-  const expected = [
-    [1, 3],
-    [2, 4],
-  ];
+    const expected = [
+      [1, 3],
+      [2, 4],
+    ];
 
-  expect(input.transpose()).toStrictEqual(expected);
-});
+    expect(input.transpose()).toStrictEqual(expected);
+  });
 
-test("transposes a rectangular matrix (more rows than columns)", () => {
-  const input = [
-    [1, 2],
-    [3, 4],
-    [5, 6],
-  ];
-  const expected = [
-    [1, 3, 5],
-    [2, 4, 6],
-  ];
+  it("transposes a rectangular matrix (more rows than columns)", () => {
+    const input = [
+      [1, 2],
+      [3, 4],
+      [5, 6],
+    ];
+    const expected = [
+      [1, 3, 5],
+      [2, 4, 6],
+    ];
 
-  expect(input.transpose()).toStrictEqual(expected);
-});
+    expect(input.transpose()).toStrictEqual(expected);
+  });
 
-test("transposes a rectangular matrix (more columns than rows)", () => {
-  const input = [
-    [1, 2, 3],
-    [4, 5, 6],
-  ];
-  const expected = [
-    [1, 4],
-    [2, 5],
-    [3, 6],
-  ];
+  it("transposes a rectangular matrix (more columns than rows)", () => {
+    const input = [
+      [1, 2, 3],
+      [4, 5, 6],
+    ];
+    const expected = [
+      [1, 4],
+      [2, 5],
+      [3, 6],
+    ];
 
-  expect(input.transpose()).toStrictEqual(expected);
-});
+    expect(input.transpose()).toStrictEqual(expected);
+  });
 
-test("returns empty array for empty matrix", () => {
-  expect([].transpose()).toEqual([]);
-});
+  it("returns empty array for empty matrix", () => {
+    expect([].transpose()).toEqual([]);
+  });
 
-test("transposes a single row matrix into a column", () => {
-  const input = [[1, 2, 3]];
-  const expected = [[1], [2], [3]];
+  it("transposes a single row matrix into a column", () => {
+    const input = [[1, 2, 3]];
+    const expected = [[1], [2], [3]];
 
-  expect(input.transpose()).toStrictEqual(expected);
-});
+    expect(input.transpose()).toStrictEqual(expected);
+  });
 
-test("transposes a single column matrix into a row", () => {
-  const input = [[1], [2], [3]];
-  const expected = [[1, 2, 3]];
+  it("transposes a single column matrix into a row", () => {
+    const input = [[1], [2], [3]];
+    const expected = [[1, 2, 3]];
 
-  expect(input.transpose()).toEqual(expected);
-});
+    expect(input.transpose()).toEqual(expected);
+  });
 
-test("works with string matrices", () => {
-  const input = [
-    ["a", "b"],
-    ["c", "d"],
-  ];
-  const expected = [
-    ["a", "c"],
-    ["b", "d"],
-  ];
+  it("works with string matrices", () => {
+    const input = [
+      ["a", "b"],
+      ["c", "d"],
+    ];
+    const expected = [
+      ["a", "c"],
+      ["b", "d"],
+    ];
 
-  expect(input.transpose()).toEqual(expected);
-});
+    expect(input.transpose()).toEqual(expected);
+  });
 
-test("it does not mutate the original matrix", () => {
-  const input = [
-    [1, 2],
-    [3, 4],
-  ];
-  const expected = [
-    [1, 3],
-    [2, 4],
-  ];
+  it("does not mutate the original matrix", () => {
+    const input = [
+      [1, 2],
+      [3, 4],
+    ];
+    const expected = [
+      [1, 3],
+      [2, 4],
+    ];
 
-  const copy = structuredClone(input);
-  const transposed = input.transpose();
+    const copy = structuredClone(input);
+    const transposed = input.transpose();
 
-  expect(input).toStrictEqual(copy);
-  expect(transposed).toStrictEqual(expected);
+    expect(input).toStrictEqual(copy);
+    expect(transposed).toStrictEqual(expected);
+  });
 });

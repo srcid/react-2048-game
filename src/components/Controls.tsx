cbc14741262ee0f3719c9686779b9344010b1cf3
Alcides Ribeiro <alcidesribeirosampaioneto@gmail.com>
2026-04-11 23:03:24 -0300

    feat(Controls.tsx): added ids to be used in tests

--- src/components/Controls.tsx
+++ src/components/Controls.tsx
@@ -1,22 +1,27 @@
+import clsx from "clsx";
 import {
   AiOutlineArrowDown,
   AiOutlineArrowLeft,
   AiOutlineArrowRight,
   AiOutlineArrowUp,
 } from "react-icons/ai";
-import { useBoardStore } from "../store/useBoardStore";
-import { MdOutlinePlusOne } from "react-icons/md";
 import { FaUndoAlt } from "react-icons/fa";
-import clsx from "clsx";
+import { MdOutlinePlusOne } from "react-icons/md";
+import { useBoardStore } from "../store/useBoardStore";
 
-export default function Controls() {
+interface ControlsProps {
+  id?: string;
+}
+
+export default function Controls({ id }: ControlsProps) {
   const boardState = useBoardStore((state) => state);
 
   return (
-    <div className="mt-2 flex flex-row gap-2">
+    <div id={id} className="mt-2 flex flex-row gap-2">
       <div className="grid grid-rows-2 grid-cols-3 gap-1 h-20 aspect-video">
         <div aria-hidden="true"></div>
         <button
+          id={id ? `${id}-btn-up` : undefined}
           className="bg-gray-700 rounded-md p-1 flex items-center justify-center"
           onClick={() => boardState.moveUp()}
         >
@@ -24,18 +29,21 @@ export default function Controls() {
         </button>
         <div aria-hidden="true"></div>
         <button
+          id={id ? `${id}-btn-left` : undefined}
           className="bg-gray-700 rounded-md p-1 flex items-center justify-center"
           onClick={() => boardState.moveLeft()}
         >
           <AiOutlineArrowLeft className="fill-zinc-100" />
         </button>
         <button
+          id={id ? `${id}-btn-down` : undefined}
           className="bg-gray-700 rounded-md p-1 flex items-center justify-center"
           onClick={() => boardState.moveDown()}
         >
           <AiOutlineArrowDown className="fill-zinc-100" />
         </button>
         <button
+          id={id ? `${id}-btn-right` : undefined}
           className="bg-gray-700 rounded-md p-1 flex items-center justify-center"
           onClick={() => boardState.moveRight()}
         >
@@ -43,12 +51,14 @@ export default function Controls() {
         </button>
       </div>
       <button
+        id={id ? `${id}-btn-addnew` : undefined}
         className="bg-gray-700 rounded-md p-1 flex items-center justify-center aspect-video"
         onClick={() => boardState.addNew()}
       >
         <MdOutlinePlusOne className="fill-zinc-100" />
       </button>
       <button
+        id={id ? `${id}-btn-undo` : undefined}
         className="bg-gray-700 rounded-md p-1 flex items-center justify-center size-20"
         onClick={() => boardState.undo()}
       >
